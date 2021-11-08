afaf1d5bfb60b7d2b55cbf356eb0ae0e3c8f0fb6
billypriv05 <93233789+billypriv05@users.noreply.github.com>
2021-11-08 19:39:43 +1300

    i did it

---
+++ bit calulater.py
@@ -0,0 +1,21 @@
+# funtions go here
+
+# puts a series of symbols at start and end of text (for emphasis)
+def statement_generator(text, decoration):
+
+    # make string with five characters
+    end = decoration * 5
+
+    # add decoration to the start and end of the statement
+    statement = "{} {} {}".format(ends, text, ends,)
+
+    print()   
+    print(statement)
+    print()
+
+    return ""
+
+# main routene goes here
+statement_generator("look-stars", "*")
+
+                  
---
+++ statment generator.py
@@ -0,0 +1,7 @@
+# funtions go here
+
+# puts a series of symbols at start and end of text (for emphasis)
+def statement_generator(text, decoration):
+
+    # make string with five characters
+    
