5c7c491502ed27af58388f86242e667ace7091df
billypriv05 <93233789+billypriv05@users.noreply.github.com>
2021-11-14 11:36:30 +1300

    done lesson 5

---
+++ text_bit_calc.py
@@ -0,0 +1,20 @@
+# calculates the # of bits for text (# of characters x 8)
+def text_bits():
+
+    print()
+    # ask user for string...
+    var_text = input("enter some text: ")
+
+    # calculate # of bits (length of string x 8)
+    var_length = len(var_text)
+    num_bits = 8 * var_length
+
+    # outputs answer with working
+    print()
+    print("/'{}/' has {} characters ...".format(var_text, var_length))  
+    print("# of bits is {} x 8...".format(num_bits, var_text))
+    print()
+
+    return ""
+# main routine goes here
+             
